ffd0972ef58e722b16e8c3ac41e2fb7d75bbdf7c
Alberto Xamin <alberto@xamin.it>
2019-01-25 23:37:01 +0100

    working wheelchair

---
+++ Assets/Scripts/WheelChair.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelChair : MonoBehaviour
+{
+    public float speed;
+    public float steer;
+    [SerializeField]
+    private Rigidbody _rigidbody;
+    void Update()
+    {
+        float fast = Input.GetAxis("Vertical") * speed;
+        float str = Input.GetAxis("Horizontal") * steer;
+        transform.Rotate(Vector3.up, str);
+        var localVel = transform.InverseTransformDirection(_rigidbody.velocity);
+        localVel.z = fast;
+        _rigidbody.velocity = transform.TransformDirection(localVel);
+    }
+}
