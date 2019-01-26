940f5d98a426661461d7c0d629db3ecd4ca71213
Alberto Xamin <alberto@xamin.it>
2019-01-26 20:48:15 +0100

    add infermiere

---
+++ Assets/Scripts/Infermiere.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Infermiere : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _agent.SetDestination(GameObject.FindWithTag("Player").transform.position);
+        _animator.SetFloat("Speed", _agent.desiredVelocity.magnitude);
+    }
+}
