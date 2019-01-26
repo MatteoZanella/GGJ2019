91635ba4bcb97b937797adaa5bf64a751b0115c3
Alberto Xamin <alberto@xamin.it>
2019-01-26 09:53:29 +0100

    nice behaviour wheelchair plus ui

--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -1,67 +1,62 @@
 ﻿using UnityEngine;
+ using System.Collections;
  
-public class SmoothFollow : MonoBehaviour
-{
-    #region Consts
-    private const float SMOOTH_TIME = 0.3f;
-    #endregion
+ // Place the script in the Camera-Control group in the component menu
+ [AddComponentMenu("Camera-Control/Smooth Follow")]
  
-    #region Public Properties
-    public bool LockX;
-    public bool LockY;
-    public bool LockZ;
-    public bool useSmoothing;
-    public Transform target;
-    #endregion
+ public class SmoothFollow : MonoBehaviour
+ {
+     /*
+     This camera smoothes out rotation around the y-axis and height.
+     Horizontal Distance to the target is always fixed.
+     
+     There are many different ways to smooth the rotation but doing it this way gives you a lot of control over how the camera behaves.
+     
+     For every of those smoothed values we calculate the wanted value and the current value.
+     Then we smooth it using the Lerp function.
+     Then we apply the smoothed values to the transform's position.
+     */
  
-    #region Private Properties
-    private Transform thisTransform;
-    private Vector3 velocity;
-    #endregion
+     // The target we are following
+     public Transform target;
+     // The distance in the x-z plane to the target
+     public float distance = 10.0f;
+     // the height we want the camera to be above the target
+     public float height = 5.0f;
+     // How much we 
+     public float heightDamping = 2.0f;
+     public float rotationDamping = 3.0f;
  
-    private void Awake()
-    {
-        thisTransform = transform;
+     void  LateUpdate ()
+     {
+         // Early out if we don't have a target
+         if (!target)
+             return;
+     
+         // Calculate the current rotation angles
+         float wantedRotationAngle = target.eulerAngles.y;
+         float wantedHeight = target.position.y + height;
+         float currentRotationAngle = transform.eulerAngles.y;
+         float currentHeight = transform.position.y;
+     
+         // Damp the rotation around the y-axis
+         currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
  
-        velocity = new Vector3(0.5f, 0.5f, 0.5f);
-    }
+         // Damp the height
+         currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
  
-// ReSharper disable UnusedMember.Local
-    private void LateUpdate()
-// ReSharper restore UnusedMember.Local
-    {
-        var newPos = Vector3.zero;
+         // Convert the angle into a rotation
+         Quaternion currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+     
+         // Set the position of the camera on the x-z plane to:
+         // distance meters behind the target
+         transform.position = target.position;
+         transform.position -= currentRotation * Vector3.forward * distance;
  
-        if (useSmoothing)
-        {
-            newPos.x = Mathf.SmoothDamp(thisTransform.position.x, target.position.x, ref velocity.x, SMOOTH_TIME);
-            newPos.y = Mathf.SmoothDamp(thisTransform.position.y, target.position.y, ref velocity.y, SMOOTH_TIME);
-            newPos.z = Mathf.SmoothDamp(thisTransform.position.z, target.position.z, ref velocity.z, SMOOTH_TIME);
-        }
-        else
-        {
-            newPos.x = target.position.x;
-            newPos.y = target.position.y;
-            newPos.z = target.position.z;
-        }
- 
-        #region Locks
-        if (LockX)
-        {
-            newPos.x = thisTransform.position.x;
-        }
- 
-        if (LockY)
-        {
-            newPos.y = thisTransform.position.y;
-        }
- 
-        if (LockZ)
-        {
-            newPos.z = thisTransform.position.z;
-        }
-        #endregion
- 
-        transform.position = Vector3.Slerp(transform.position, newPos, Time.time);
-    }
-}
+         // Set the height of the camera
+         transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+     
+         // Always look at the target
+         transform.LookAt (target);
+     }
+ }
