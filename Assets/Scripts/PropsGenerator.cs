642ee72adeb93ee9410cb94f308d4b14aafbb804
Matteo Zanella <matteo.zanella-2@studenti.unitn.it>
2019-01-27 14:56:09 +0100

    add room props generator

--- Assets/Scripts/PropsGenerator.cs
+++ Assets/Scripts/PropsGenerator.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PropsGenerator : MonoBehaviour
 {
+    public GameObject[] propsTemplates;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(propsTemplates[Random.Range(0,propsTemplates.Length)], transform);
     }
 
     // Update is called once per frame
