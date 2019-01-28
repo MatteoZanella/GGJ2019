b5313071a64a139cabe351c5d6c4c35c5e2cc3ba
Giulio Migani <giulio97.migani@gmail.com>
2019-01-28 02:40:52 +0100

    Partial algorithm
    The algorithm still does not work correctly

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
 
+//public enum Position { N, S, W, E };
 public class Room : MonoBehaviour
 {
-    public Transform N, S, W, E;
+    public Transform N, S, W, E;//N=0, S=1, W=2, E=3
     public byte[] openings = new byte[4];//NSWE
     [SerializeField] private NavMeshSurface _surface;
-    public List<Room> neighboors = new List<Room>();
+    public Room[] Neighbour = new Room[4];
+    public bool CanChangeDistance;
+    public Vector3 Posizione { get; set; }
 
-    public UnityAction Enter;
+    private int dinstanceFromPlayer;
+    public int DinstaceFromPlayer
+    {
+        get { return dinstanceFromPlayer; }
+        set
+        {
+            if (CanChangeDistance)
+            {
+                dinstanceFromPlayer = value;
+                CanChangeDistance = false;
+            }
+        }
+    }
+
+    public UnityAction AdaptiveRandom;
+    public UnityAction ChangeDinstace;
+    public UnityAction<int> aaa;
 
     private void GetWallOpen(Transform t, byte on)
     {
-        if (on == 1) t.GetChild(1).gameObject.SetActive(false); 
-        else t.GetChild(0).gameObject.SetActive(false); 
+        if (on == 1) //1 == door
+        {//t0: door t1: wall
+            t.GetChild(1).gameObject.SetActive(false);
+            t.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            t.GetChild(0).gameObject.SetActive(false);
+            t.GetChild(1).gameObject.SetActive(true);
+        }
     }
 
     public void Init()
@@ -25,12 +52,97 @@ public class Room : MonoBehaviour
         GetWallOpen(S, openings[1]);
         GetWallOpen(W, openings[2]);
         GetWallOpen(E, openings[3]);
-        _surface.BuildNavMesh();
+        //_surface.BuildNavMesh();
+    }
+
+    public static int GetOpposite(int from)
+    {
+        switch (from)
+        {
+            case 0:
+                return 1;
+            case 1:
+                return 0;
+            case 2:
+                return 3;
+            case 3:
+                return 2;
+            default: return -1;
+        }
+    }
+
+    public int DoorCount()
+    {
+        int num = 0;
+        for (int i = 0; i < openings.Length; i++)        
+            if (openings[i] == 1)
+                num++;
+        
+        return num;
+    }
+
+    public void GenerateWall(int from = -1)
+    {
+        int minimo = 2;
+        if (from == -1)
+            minimo = 3;
+        do
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                //N=0, S=1, W=2, E=3
+                if (i != Room.GetOpposite(from))
+                    openings[i] = (byte)(Random.Range(0, 10) < 6 ? 1 : 0);
+            }
+        } while (DoorCount() < minimo);
+    }
+
+    public void Delete()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (Neighbour[i] != null)
+                Neighbour[i].Neighbour[GetOpposite(i)] = null;
+
+        }
+        Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") || other.CompareTag("GameController"))
-            Enter();
+        {
+            if (dinstanceFromPlayer != 0)
+            {
+                Debug.Log("Chiamato");
+                ChangeDinstace();
+                dinstanceFromPlayer = 0;
+                CanChangeDistance = false;
+
+                for (int i = 0; i < Neighbour.Length; i++)
+                {
+                    if (Neighbour[i] != null && Neighbour[i].CanChangeDistance) //se il vicino c'è ed è modificabile
+                    {
+                        Neighbour[i].DinstaceFromPlayer = 1; //cambio la distanza del mio vicino
+                        for (int l = 0; l < Neighbour[i].Neighbour.Length; l++)
+                        {
+                            if (Neighbour[i].Neighbour[l] != null && Neighbour[i].Neighbour[l].CanChangeDistance)
+                            { //se il vicino del mio vicino c'è ed è modificabile
+                                Neighbour[i].Neighbour[l].DinstaceFromPlayer = 2; //cambio la distanza del vicino del mio vicino
+                                for (int j = 0; j < Neighbour[i].Neighbour[l].Neighbour.Length; j++)
+                                {
+                                    if (Neighbour[i].Neighbour[l].Neighbour[j] != null && Neighbour[i].Neighbour[l].Neighbour[j].CanChangeDistance) //se il vicino del vicino del mio vicino c'è ed è modificabile
+                                        Neighbour[i].Neighbour[l].Neighbour[j].DinstaceFromPlayer = 3; //cambio la distanza del vicino del vicino del mio vicino
+                                }
+                            }
+                        }
+                    }
+                }
+                //name = "Room " + dinstanceFromPlayer;
+                AdaptiveRandom();
+            }
+        }
+        
     }
 }
+
--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -9,56 +9,103 @@ public class WorldGenerator : MonoBehaviour
     public GameObject RoomPrefab;
     public Vector2 gridSize;
     public Transform player;
+    public List<Room> rooms;
+    private int numero = 0;
 
     void Awake()
     {
         instance = this;
+        rooms = new List<Room>();
         Generate();
     }
 
-    int GetOpposite(int from)
+    void Generate(Vector3 pos = new Vector3(), int from = -1, Room origin = null)
     {
-        switch (from)
-        {
-            case 0:
-                return 1;
-            case 1:
-                return 0;
-            case 2:
-                return 3;
-            case 3:
-                return 2;
-            default: return -1;
-        }
-    }
-    
-    void Generate(Vector3 pos = new Vector3(), int deep = 0, int from = -1, Room origin = null)
-    {
-        if (deep >= genDistance) return;
+        Debug.Log("genero " +numero);
         GameObject roomObj = Instantiate(RoomPrefab, pos, Quaternion.identity);
-        roomObj.name = "Room" + deep;
+        roomObj.name = "Room " + numero;
+        numero++;
         Room room = roomObj.GetComponent<Room>();
-        for (int i = 0; i < 4; i++)
+        rooms.Add(room);//aggiungo la room alla lista di tutte le room
+        room.Posizione = pos; //salvo la posizione della room dentro alla stessa
+        if (from != -1) //se la room è stata generata da un'altra; quindi a rigor di logica non è la prima
         {
-            room.openings[i] = (byte) (Random.Range(0, 10) < 6? 1:0);
-            if (GetOpposite(from) != i && room.openings[i]==1)
-                Generate(//N=0, S=1, W=2, E=3
-                    pos + new Vector3((i < 2 ? 0 : gridSize.x * (i == 2 ? -1 : 1)),0,
-                        (i < 2 ? gridSize.y * (i == 0 ? 1 : -1) : 0)), deep + 1, i, room);
+            room.openings[Room.GetOpposite(from)] = (byte) 1; //la porta comunicante dell'origine deve essere accoppiata con una porta nella stanza adiacente
+            origin.Neighbour[from] = room; //l'orgine mi ha come vicino nella sua porta comunicante con me
+            room.Neighbour[Room.GetOpposite(from)] = origin; // io ho come vicino l'origine nella porta accoppiata
+            room.CanChangeDistance = true; //cambio la distanza
+            room.DinstaceFromPlayer = origin.DinstaceFromPlayer + 1; //la distanza dal player è data dalla distanza che ha l'origine + 1
+            if (room.DinstaceFromPlayer == 2) //se la  la distanza dal player è arrivata a 2 vuol dire che non ne devo generare altre stanze
+            {
+                room.Init();//termina generando la struttura della stanza
+                return;
+            }
+            room.GenerateWall(from);
         }
-
-        if (from != -1)
-        {
-            room.openings[GetOpposite(from)] = 1;
-            origin.neighboors.Add(room);
+        else
+        { // questa stanza è la stanza radice, la prima generata
+            room.CanChangeDistance = true;
+            room.DinstaceFromPlayer = 0;
+            room.GenerateWall();
         }
-
-        room.Enter = delegate
-        {
-            Debug.Log("Entered in room " + room.name);
-//            if (deep > 1)
-                
-        };
+        for (int i = 0; i < 4; i++)//N=0, S=1, W=2, E=3
+        { 
+            if (Room.GetOpposite(from) != i && room.Neighbour[i] == null && room.openings[i] == 1)
+                Generate(pos + new Vector3((i < 2 ? 0 : gridSize.x * (i == 2 ? -1 : 1)), 0,
+                        (i < 2 ? gridSize.y * (i == 0 ? 1 : -1) : 0)), i, room);
+        }
         room.Init();
+
+        room.AdaptiveRandom = delegate
+        {
+            List<Room> iterator = new List<Room>();
+            iterator.AddRange(rooms);
+            foreach (Room item in iterator)
+            {
+                if (item.DinstaceFromPlayer > 2)
+                {
+                    rooms.Remove(item);
+                    item.Delete();
+                }
+                else if (item.DinstaceFromPlayer == 2)
+                {
+                    int nearest = 0;
+                    Room r = new Room();
+                    r.CanChangeDistance = true;
+                    r.DinstaceFromPlayer = 5;
+                    for (int i = 1; i < 4; i++)
+                    {                        
+                        if (item.Neighbour[i] != null && item.Neighbour[i].DinstaceFromPlayer < ((item.Neighbour[nearest] == null) ? r.DinstaceFromPlayer: item.Neighbour[nearest].DinstaceFromPlayer))
+                            nearest = i;
+                    }
+                    item.GenerateWall(nearest);
+                    item.Init();
+                }
+                else if (item.DinstaceFromPlayer == 1)
+                {
+                    for (int i = 0; i < item.Neighbour.Length; i++)
+                    {
+                        if (item.openings[i] == 1 && item.Neighbour[i] == null)
+                            Generate(item.Posizione, i, item);
+                    }
+                }
+            }
+        };
+        room.ChangeDinstace = delegate
+        {
+            foreach (Room item in rooms)
+            {
+                if (item == null)
+                {
+
+                }
+                item.CanChangeDistance = true;
+            }
+        };
+
+        room.aaa = delegate (int a)
+        {
+
+        };
     }
 }
